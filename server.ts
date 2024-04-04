5ee5b5e2b8dc1a07984c7d326f45cef0f5ace5ee
vipin <vipinrj8473@gmail.com>
2024-04-04 18:18:03 +0530

    added the socketio

--- server.ts
+++ server.ts
@@ -39,16 +39,16 @@ interface UserSocket {
 const app: express.Application = express();
 
 const http = require("http").Server(app);
+
 const io = require("socket.io")(http, {
   cors: {
-    // origin: "http://localhost:3000",
+    origin: "*",
   },
 });
-cors;
+
 app.use(
   cors({
-    // origin: "http://localhost:3000",
-    // credentials: true,
+    origin: "*",
   })
 );
 
